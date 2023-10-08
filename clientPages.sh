50788f0677d62dfdd2d30a0a82058442172c1695
3000ye <87135443+3000ye@users.noreply.github.com>
2023-10-08 15:07:31 +0000

    test clientPages.sh

---
+++ clientPages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 检查是否提供了提交备注作为参数
+if [ $# -eq 0 ]; then
+  echo "请提供提交备注作为参数。"
+  exit 1
+fi
+
+# 提取提交备注
+commit_message="$1"
+# 添加所有更改
+git add .
+# 提交更改并附带提交备注
+git commit -m "$commit_message"
+
+# 推送更改到远程仓库（假设你已经设置了远程仓库）
+git push origin main
+
+echo "Pages clinet completed!"
